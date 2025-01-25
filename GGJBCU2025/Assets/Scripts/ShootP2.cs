8ad5a460215a1e8be354fc7aea06872b2fc21c9c
AlexandtheSackboys <a.j.costin1@gmail.com>
2025-01-25 14:24:13 +0000

    player 2 shoot
    added a script that allows player 2 to have a different input when shooting

---
+++ GGJBCU2025/Assets/Scripts/ShootP2.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+
+public class ShootP2 : MonoBehaviour
+{
+    public float Speed = 1f;
+    float fireRate = 0;
+    float fireDelay = 0.5f;
+    public Rigidbody projectile;
+    public float Cooldown;
+    public GameObject BubblePrefab;
+    public Transform Spawner;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    void Bubble_Shoot()
+    {
+        if (Time.time < fireRate) return;
+        BubblePrefab.transform.position = Spawner.transform.position;
+        Rigidbody BubbleRb = Instantiate(projectile, new Vector3(transform.position.x, transform.position.y, transform.position.z), gameObject.transform.rotation) as Rigidbody;
+        BubbleRb.AddForce(transform.forward * 500 * Speed); // indicates the direction and level of force
+        fireDelay = Time.time + Cooldown;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time > fireDelay)
+        {
+            // Ctrl was pressed, launch a projectile
+            if (Input.GetKeyDown(KeyCode.Comma))
+            {
+                Bubble_Shoot();
+
+            }
+
+        }
+    }
+}
