d51d07da42927f24ea20c8ce793eb5ba808781a7
rockhandle1 <122266267+rockhandle1@users.noreply.github.com>
2025-01-25 17:10:48 +0000

    Victory function

---
+++ GGJBCU2025/Assets/Scripts/mikescripts/Victory.cs
@@ -0,0 +1,43 @@
+using NUnit.Framework;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Player
+{
+    public GameObject player;
+    public bool alive = true;
+}
+public class Victory : MonoBehaviour
+{
+    List<Player> players = new();
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        players.Add(new Player { player = GameObject.FindGameObjectWithTag("Player") });
+        players.Add(new Player { player = GameObject.FindGameObjectWithTag("Player2") });
+        Debug.Log(players.Count);
+    }
+    bool called = false;
+    void victory()
+    {
+        if (called) return;
+        called = true;
+        for (int i = 0; i < players.Count; i++)
+        {
+            if (!players[i].alive) continue;
+            Debug.Log(players[i].player.tag + " Wins");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i = 0; i < players.Count; i++)
+        {
+            if (!players[i].player) { players[i].alive = false; }
+            else { continue; }
+            victory();
+        }
+    }
+}
