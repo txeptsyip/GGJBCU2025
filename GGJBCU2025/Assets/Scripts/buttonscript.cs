4549430bd47ab2a724f977170d3d7d64fa78b572
txeptsyip <lekoro@hotmail.co.uk>
2025-01-26 14:13:24 +0000

    music now persists onto end screen and then does not past that poi9nt

--- GGJBCU2025/Assets/Scripts/buttonscript.cs
+++ GGJBCU2025/Assets/Scripts/buttonscript.cs
@@ -3,19 +3,24 @@ using UnityEngine.SceneManagement;
 
 public class buttonscript : MonoBehaviour
 {
+
+    private GameObject musicobject;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        musicobject = GameObject.FindGameObjectWithTag("Music");
     }
 
     public void Play() //  causes main game to play
     {
+        Destroy(musicobject);
         SceneManager.LoadScene(1);
+
     }
 
     public void ResetGame()
     { // reset to the games title screen
+        Destroy(musicobject);
         SceneManager.LoadScene(0);
         Debug.Log("reset presses i guess");
     }
